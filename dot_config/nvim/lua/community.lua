5eb370d5f2e45299f6a752e378858ba887747f17
stasshe <133995608+Stasshe@users.noreply.github.com>
2026-06-28 19:43:17 +0900

    md-preview

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -1,6 +1,8 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
   { import = "astrocommunity.scrolling.nvim-scrollbar" },
 
   { import = "astrocommunity.recipes.picker-nvchad-theme" },
