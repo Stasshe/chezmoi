54dbec625fa49bcc15a5c93a8bb566dcaa4e755b
Stasshe <133995608+Stasshe@users.noreply.github.com>
2026-07-14 23:52:55 +0900

    c-s-f nvim

--- dot_config/nvim/lua/plugins/picker.lua
+++ dot_config/nvim/lua/plugins/picker.lua
@@ -6,6 +6,10 @@ return {
       n = {
         ["<C-p>"] = { function() require("snacks").picker.files() end, desc = "Find files" },
         ["<leader>fi"] = { function() require("snacks").picker.highlights() end, desc = "Find highlights" },
+        ["<C-S-f>"] = { function() require("snacks").picker.grep() end, desc = "Find words" },
+      },
+      x = {
+        ["<C-S-f>"] = { function() require("snacks").picker.grep() end, desc = "Find words" },
       },
     },
   },
