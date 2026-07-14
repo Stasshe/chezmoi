d26e8ca4f5b7e3b24673bf7befe723142c50b019
stasshe <133995608+Stasshe@users.noreply.github.com>
2026-07-14 22:01:18 +0900

    double

--- dot_config/hypr/hyprland/execs.lua
+++ dot_config/hypr/hyprland/execs.lua
@@ -2,14 +2,12 @@
 hl.on("hyprland.start", function ()
 
     -- Bar, wallpaper
-    hl.exec_cmd("$HOME/.config/hypr/hyprland/scripts/start_geoclue_agent.sh")
     hl.exec_cmd("gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'")
     hl.exec_cmd("gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark'")
     hl.exec_cmd("qs -c $qsConfig")
     hl.exec_cmd("$HOME/.config/hypr/custom/scripts/__restore_video_wallpaper.sh")
 
-    -- Core components (authentication, lock screen, notification daemon)
-    hl.exec_cmd("gnome-keyring-daemon --start --components=secrets")
+    -- Core components (lock screen, notification daemon)
     hl.exec_cmd("hypridle")
     hl.exec_cmd("dbus-update-activation-environment --all")
     hl.exec_cmd("sleep 1 && dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP") -- Some fix idk
