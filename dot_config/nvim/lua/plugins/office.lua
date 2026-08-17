857345e192f6eb4f8bd56248a3d016eac680fa40
Stasshe <133995608+Stasshe@users.noreply.github.com>
2026-08-18 01:04:04 +0900

    nvim-office error handling

--- dot_config/nvim/lua/plugins/office.lua
+++ dot_config/nvim/lua/plugins/office.lua
@@ -1,6 +1,10 @@
+local plugin_dir = vim.fn.expand("~/dev/github/nvim-office")
+
+if vim.fn.isdirectory(plugin_dir) == 0 then return {} end
+
 return {
   {
-    dir = vim.fn.expand("~/dev/github/nvim-office"),
+    dir = plugin_dir,
     name = "nvim-office",
     lazy = false,
     dependencies = { "folke/snacks.nvim" },
